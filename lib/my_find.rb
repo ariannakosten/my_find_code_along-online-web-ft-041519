20a2b21830d81211fa530f82dc561a1b580187f6
Arianna Kosten <arianna.kosten@gmail.com>
2019-04-24 19:58:40 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,9 +1,9 @@
 require 'pry'
 
-
 def my_find(collection)
   i = 0
   while i < collection.length
+  #confused at what line 7 is doing.
     return collection[i] if yield(collection[i])
     i = i + 1
   end
